0492d30ae92556166165042e3deb51afcf020c32
Vitaly <mirenrich@yandex.ru>
2023-08-02 12:46:51 +0300

    Updates

---
+++ static/js/180.d29653fd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktreact=self.webpackChunktreact||[]).push([[180],{9180:function(e,o,t){t.r(o);var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof t.g?n=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}}]);
+//# sourceMappingURL=180.d29653fd.chunk.js.map
