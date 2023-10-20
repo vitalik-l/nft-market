ec38f8dc348dbfc2ba8670a23858faa494401a21
Vitaly <mirenrich@yandex.ru>
2023-10-20 14:33:36 +0300

    config in window

--- src/shared/config/index.js
+++ src/shared/config/index.js
@@ -1,3 +1,5 @@
 export const MaxUint256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 
 export const STRAPI_URL = process.env.REACT_APP_STRAPI_URL;
+
+window.config = { STRAPI_URL };
