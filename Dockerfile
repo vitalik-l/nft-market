96f5793e1ff6688b881a0105a1e0c232e15a4cd7
Vitaly <mirenrich@yandex.ru>
2023-10-30 19:00:54 +0300

    updates style

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM nginx:1.25.2-alpine
 RUN apk update && apk add --no-cache geoip-dev nginx-mod-http-geoip > /dev/null 2>&1
+RUN apk update && apk add --no-cache nginx-mod-http-geoip > /dev/null 2>&1
+WORKDIR /etc/nginx
+RUN wget https://centminmod.com/centminmodparts/geoip-legacy/GeoIP.dat.gz
+RUN gunzip GeoIP.dat.gz
 WORKDIR /usr/share/nginx/html
 RUN rm -rf ./*
 COPY build/ .
--- src/pages/nft/index.js
+++ src/pages/nft/index.js
@@ -35,7 +35,7 @@ const TextContent = tw.div`lg:py-8 text-center md:text-left`;
 const Heading = tw(
   SectionHeading
 )`mt-4 font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
-const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`;
+const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100 whitespace-break-spaces`;
 
 const Statistics = tw.div`flex flex-wrap items-center sm:block mt-4 gap-[2rem]`;
 const Statistic = tw.div`text-left sm:inline-block sm:mr-12 last:mr-0 mt-4`;
