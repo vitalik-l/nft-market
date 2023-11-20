80d2dda1b920be6fc01682d937675fa6d96729f2
Vitaly <mirenrich@yandex.ru>
2023-11-20 14:58:19 +0300

    nft gallery abi updated

--- src/shared/abi/index.js
+++ src/shared/abi/index.js
@@ -1,3 +1,2 @@
-export { default as galleryFactoryAbi } from './GalleryFactoryAbi.json';
 export { default as nftAbi } from './NftGalleryAbi.json';
 export { default as erc20Abi } from './ERC20.json';
